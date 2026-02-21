ee26574985ebdcd2ee84ccdc04556e09fc7762d8
unknown <you@example.com>
2026-02-21 15:30:53 -0800

    add program

--- side1.py
+++ side1.py
@@ -4,3 +4,12 @@ user_age1=23
 user_age2=24
 User_age=73
 print(user_age,user_age1,user_age2,User_age)
+
+
+
+# variable understanding
+student_name="ali"
+student_name1="akram"
+student_name2="hassan"
+Student_name="ahsan"
+print(student_name,student_name1,student_name2,Student_name)
