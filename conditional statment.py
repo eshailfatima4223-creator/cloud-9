4108292e48912d1e2d00c6faa62ad7d9db6c36f6
unknown <you@example.com>
2026-02-25 15:37:46 -0800

    cloud

--- conditional statment.py
+++ conditional statment.py
@@ -2,3 +2,17 @@ if 5>10:
     print("5 is grater than 10")
 if 999>998:
     print("999 is grater than 998")
+
+
+
+
+
+
+marks=float(input("enter your marks:"))
+
+if marks>50:
+    print("congratulation you are passed", marks)
+    persentage=marks/100*100
+    print("your persentage is:", persentage , "%")
+
+print("program end")
