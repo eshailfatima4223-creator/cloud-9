1f4b55778a90eeaa26f312ffd18edee575b6d41c
unknown <you@example.com>
2026-02-25 15:08:05 -0800

    cloud

---
+++ multiply_ two_ number.py
@@ -0,0 +1,4 @@
+num1=int(input("enter first number to multiply"))
+num2=int(input("enter second number to multiply"))
+result=num1*num2
+print("the result of two number multiplication")
--- side1.py
+++ side1.py
@@ -11,8 +11,22 @@ print(user_age,user_age1,user_age2,User_age)
 # variable understanding
 student_name="sadia"
 student_name1="alisha"
-student_name2="malika"
 Student_name="amna"
 print(student_name,student_name1,student_name2,Student_name)
 
+# variable understanding
 
+student_name= "hassan"
+student_name1= "mohsin"
+student_name2= "arham"
+print(student_name,student_name1,student_name2)
+
+
+
+
+#write a program that get two integer value from the user and multiply to output result
+
+num1= int("enter first num1 to multiply")
+num2= int("enter second num2 to multiply")
+res=num1*num2 
+print(multiply)
